f2655cfcb6ba26d189478a73e2226f461be5a50d
Curtis <excentriceevee@hotmail.com>
2021-12-25 15:16:09 -0500

    Resolved an issue with Webpacker/Node

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+    before_action :require_login, only: [:new, :create]
+    #before_action :authenticate_user!
+
+    def index
+        @posts
+    end
+
+    def new
+
+    end
+
+    private
+
+    def require_login
+        unless logged_in?
+            flash[:error] = "You must be logged in to do that."
+            redirect_to new_user_session
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   #placeholder until we get some models in
-  root "home#index"
+  root "posts#index"
   resources :posts, only: [:new, :create, :index]
 end
