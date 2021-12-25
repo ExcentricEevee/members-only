d31cbd0ed2777fa40f8c149e2e0eedce49aa258c
Curtis <excentriceevee@hotmail.com>
2021-12-25 13:32:38 -0500

    Create User and Post models, beginning Posts controller and routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  #placeholder until we get some models in
+  root "home#index"
+  resources :posts, only: [:new, :create, :index]
 end
